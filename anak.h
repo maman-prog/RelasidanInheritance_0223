cb3afc3774bbe041cbdf507561b62386caa4d667
Maman <ingratarmanrizky19@gmail.com>
2026-05-11 17:51:54 +0700

    class anak.h

--- anak.h
+++ anak.h
@@ -1,2 +1,17 @@
-#ifndef  ANAK_H
+#ifndef ANAK_H
 #define ANAK_H
+
+#include <iostream>
+using namespace std;
+
+class anak {
+public:
+    string nama;
+    anak(string pNama) : nama(pNama) {
+        cout << "Anak " << nama << " ada" << endl;
+    }
+    
+    ~anak() {
+        cout << "Anak " << nama << " tidak ada" << endl;
+    }
+};
