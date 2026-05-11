034df4497564274bab403715f125f52a62193a5d
Maman <ingratarmanrizky19@gmail.com>
2026-05-11 19:31:22 +0700

    int main composisi

--- composisi.cpp
+++ composisi.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+#include "jantung.h"
+#include "manusia.h"
+
+int main()
+{
+    //deklarasi dan pemberian nilai object pointer varManusia
+    Manusia* varManusia = new Manusia("jono");
+    //mendelete pointer varManusia dari memori
+    delete varManusia;
+    return 0;
+}
