d0553b7c43ab22b1d907920efb410ee7dfab4cd9
Maman <ingratarmanrizky19@gmail.com>
2026-05-11 20:04:22 +0700

    sesi void

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -18,4 +18,32 @@ public:
 
     ~pasien() {
         cout << "Pasien \"" << nama << "\" tidak ada\n";
+
+//deklarasi prosedur tambahDokter() dan cetakDokter()
+    void tambahDoker(dokter*);
+    void cetakDokter();
+
+};
+
+//pembuatan class dokter
+class dokter {
+public:
+    //deklarasi member variabel dari class dokter
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    //pembuatan constructor dan destructor dari class dokter
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    //deklarasi prosedur tambahPasien() dan cetakPasien()
+    void tambahPasien(pasien*);
+    void cetakPasien();
+
+};
     
