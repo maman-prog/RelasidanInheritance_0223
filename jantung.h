7b9778cbb7618be2916447994738c5078f5f2159
Maman <ingratarmanrizky19@gmail.com>
2026-05-11 19:25:06 +0700

    class jantung

--- jantung.h
+++ jantung.h
@@ -1,2 +1,13 @@
 #ifndef JANTUNG_H
 #define JANTUNG_H
+
+class Jantung {
+public:
+    Jantung() {
+        cout << "Jantung Ditambah\n";
+    }
+    ~Jantung() {
+        cout << "Jantung Dimatikan\n";
+    }
+};
+#endif
