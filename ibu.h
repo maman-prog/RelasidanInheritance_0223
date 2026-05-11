8153e4b99e52fe494e8cb912ab85de566f73b685
Maman <ingratarmanrizky19@gmail.com>
2026-05-11 18:38:09 +0700

    class void ibu

--- ibu.h
+++ ibu.h
@@ -1,17 +1,37 @@
 #ifndef IBU_H
 #define IBU_H
-#endif
+#include <iostream>
+#include <vector>
+#include <string>
+#include "anak.h"
+using namespace std;
 
 class anak;
-
-//membuat class ibu pada file header ibu.h
-class Ibu {
+class ibu {
 public:
-    //deklarasi member variabel pada class ibu
     string nama;
-    vector<anak> daftar_anak;
+    vector<anak*> DaftarAnak;
 
-    //membuat constructor dan destructor pada class ibu
-    Ibu(string pNama) :nama(pNama) {
-        cout << "Ibu "" << nama << "" ada\n";
+    ibu(string pNama) : nama(pNama) {
+        cout << "Ibu " << nama << " ada" << endl;
     };
+~ibu() {
+        cout << "Ibu " << nama << " tidak ada" << endl;
+    };
+
+    void tambahAnak(anak* pAnak);
+    void cetakAnak();
+};
+
+void ibu::tambahAnak(anak* pAnak) {
+    DaftarAnak.push_back(pAnak);
+}
+
+void ibu::cetakAnak() {
+    cout << "Daftar anak dari Ibu " << nama << ":" << endl;
+    for (size_t i = 0; i < DaftarAnak.size(); i++) {
+        cout << "- " << DaftarAnak[i]->nama << endl;
+    }
+}
+
+#endif
