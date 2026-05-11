23dc29a2c3e9dbb899f3020ad9ccaf0add6e9859
Maman <ingratarmanrizky19@gmail.com>
2026-05-11 18:35:16 +0700

    class anak

--- ibu.h
+++ ibu.h
@@ -1,3 +1,17 @@
 #ifndef IBU_H
 #define IBU_H
 #endif
+
+class anak;
+
+//membuat class ibu pada file header ibu.h
+class Ibu {
+public:
+    //deklarasi member variabel pada class ibu
+    string nama;
+    vector<anak> daftar_anak;
+
+    //membuat constructor dan destructor pada class ibu
+    Ibu(string pNama) :nama(pNama) {
+        cout << "Ibu "" << nama << "" ada\n";
+    };
